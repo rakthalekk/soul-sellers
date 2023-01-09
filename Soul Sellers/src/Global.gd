78358196569bfcc668867c62db4ad3185e21509e
mmedding <max.meddington29@gmail.com>
2023-01-09 00:23:26 -0500

    Purchasable Upgrades

--- Soul Sellers/src/Global.gd
+++ Soul Sellers/src/Global.gd
@@ -1,9 +1,13 @@
 extends Node
 
+var grave_unlock_flag = false
+var void_unlock_flag = false
+var coffin_unlock_flag = false
+var homing_souls_unlock_flag = false
 
 var slime_souls = 0
 var ghost_souls = 0
 var zombie_souls = 0
 var vampire_souls = 0
 var reaper_souls = 0
-var money_count = 0
+var money_count = 5000
